63413cd7998c976c030e7763494e52f6a5343c67
ViníciusYoda <rm92969@fiap.com.br>
2022-05-25 17:43:58 -0300

    Uptade script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ Enviar.addEventListener("click", ()=>{
    const campos = document.querySelectorAll("input[type='text'],input[type='email'],input[type='date'],select[option],textarea")
     
 
-     for(let x = 0; x < campos.length ; x++){=
-        
+     for(let x = 0; x < campos.length ; x++){
+
            
         if(campos[x].value == ""){
             alert("Os campos estão vazios! Preencha todos correntamente")
