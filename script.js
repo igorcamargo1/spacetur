9751b7907fc23f94f5b5bb7d2813b7d45024d6ed
igorcamargo1 <rm94553@fiap.com.br>
2022-05-26 09:01:49 -0300

    começando functon javascript

--- script.js
+++ script.js
@@ -1,19 +1,57 @@
-const Enviar = document.getElementById("enviar")
+/* função validar */
 
-Enviar.addEventListener("click", ()=>{
+function validar(){
+    //pegando o valor pelos nomes
 
-   const campos = document.querySelectorAll("input[type='text'],input[type='email'],input[type='date'],select[option],textarea")
-    
+    var nome = document.getElementById("nmId")
+    var sobrenome = document.getElementById("sobrenomeId")
+    var email = document.getElementById("emailId")
+    var cpf = document.getElementById("cpfId")
+    var genero = document.getElementById("generoId")
+    var data = document.getElementById("dateId")
+    var motivo = document.getElementById("motivoId")
+   
 
-     for(let x = 0; x < campos.length ; x++){
+    // verificar se o nome está vazio
+  if (nome.value == "") {
+    alert("Nome não informado");
 
-           
-        if(campos[x].value == ""){
-            alert("Os campos estão vazios! Preencha todos correntamente")
-            return false            
-        }else{
-            alert("Campos preenchidos ☻")
-            return true
-        }
-    }
-})
+    // Deixa o input com o focus
+    nome.focus();
+    // retorna a função e não olha as outras linhas
+    return;
+  }
+  if (sobrenome.value == "") {
+    alert("Sobrenome não informado");
+    sobrenome.focus();
+    return;
+  }
+  if (email.value == "") {
+    alert("E-mail não informado");
+    email.focus();
+    return;
+  }
+  if (cpf.value == "") {
+    alert("Senha não informada");
+    cpf.focus();
+    return;
+  }
+  if (genero.value == "") {
+    alert("Telefone não informado");
+    genero.focus();
+    return;
+  }
+  if (data.value == "") {
+    alert("CEP não informado");
+    data.focus();
+    return;
+  }
+  if (motivo.value == "") {
+    alert("CEP não informado");
+    motivo.focus();
+    return;
+  }
+  alert("Formulário enviado!");
+  // envia o formulário
+  //formulario.submit();
+}
