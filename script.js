03140d421419463deac279da737d3ca9443130a3
ViníciusYoda <rm92969@fiap.com.br>
2022-05-27 17:02:39 -0300

    finalizando o js

--- script.js
+++ script.js
@@ -1,58 +1,14 @@
-/* função validar */
+const botaoEnviar = document.getElementById("enviar")
 
-function validar(){
-    //pegando o valor pelos nomes
+botaoEnviar.addEventListener("click", ()=>{
+  const campos = document.querySelectorAll("input[type='text'],input[type='email'],select[type='option'],input[type='date'],textarea")
 
-    var nome = document.getElementById("nmId")
-    var sobrenome = document.getElementById("sobrenomeId")
-    var email = document.getElementById("emailId")
-    var cpf = document.getElementById("cpfId")
-    var genero = document.getElementById("generoId")
-    var data = document.getElementById("dateId")
-    var motivo = document.getElementById("motivoId")
-   
+  for(let x = 0; x < campos.length; x++){
 
-    // verificar se o nome está vazio
-  if (nome.value == "") {
-    alert("Nome não informado");
-
-    // Deixa o input com o focus
-    nome.focus();
-    // retorna a função e não olha as outras linhas
-    return false;
+    if(campos[x].value == ""){
+      alert("O campo " + campos[x].name + " está sem preenchimento!")
+      return
+    }
   }
-  if (sobrenome.value == "") {
-    alert("Sobrenome não informado");
-    sobrenome.focus();
-    return false;
-  }
-  if (email.value == "") {
-    alert("E-mail não informado");
-    email.focus();
-    return false;
-  }
-  if (cpf.value == "") {
-    alert("Senha não informada");
-    cpf.focus();
-    return false;
-  }
-  if (genero.value == "") {
-    alert("Telefone não informado");
-    genero.focus();
-    return false;
-  }
-  if (data.value == "") {
-    alert("CEP não informado");
-    data.focus();
-    return false;
-  }
-  if (motivo.value == "") {
-    alert("CEP não informado");
-    motivo.focus();
-    return false;
-  }
-  alert("Formulário enviado!");
-  return;
-  // envia o formulário
-  //formulario.submit();
-}
+  document.getElementsByClassName("formulario")[0].submit()
+})
